3f594f21bcbd64be3a0da83d2ddb5b11c6309711
Daniel Shimmyo <dshimmyo@gmail.com>
2026-03-28 15:41:02 -0400

    paddle controller first pass

--- src/gte.cpp
+++ src/gte.cpp
@@ -88,6 +88,7 @@ int resetQueued = 0;
 #define MUTE_SOURCE_MANUAL 1
 #define MUTE_SOURCE_MENU 2
 int muteMask = 0;
+bool paddle_emulation_enabled = true;
 
 void SaveNVRAM() {
 	fstream file;
@@ -1433,6 +1434,31 @@ int main(int argC, char* argV[]) {
 #else
 	SDL_RaiseWindow(mainWindow);
 	while(running) {
+
+        if (paddle_emulation_enabled) {
+            int mx, my, winW, winH;
+            SDL_GetMouseState(&mx, &my);
+            SDL_GetWindowSize(mainWindow, &winW, &winH);
+
+            // 1. Calculate the 8-bit position (0-255) and invert it
+            uint8_t p = ~((uint8_t)((mx * 255) / winW));
+
+            // 2. Map the bits to your custom 16-bit mask
+            uint16_t paddle_state = 0;
+            if (p & 0x01) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_UP;
+            if (p & 0x02) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_DOWN;
+            if (p & 0x04) paddle_state |= GameTankButtons::GamepadButtonMask::LEFT;
+            if (p & 0x08) paddle_state |= GameTankButtons::GamepadButtonMask::RIGHT;
+            if (p & 0x10) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_X;
+            if (p & 0x20) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_Y;
+            if (p & 0x40) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_Z;
+            if (p & 0x80) paddle_state |= GameTankButtons::GamepadButtonMask::PADDLE_MODE;
+
+            // 3. Update the joystick state (Port 0)
+            //joysticks->SetJoystickState(0, paddle_state);
+			joysticks->SetHeldButtons(paddle_state);
+        }
+
 		mainloop(0, NULL);
 	}
 	joysticks->SaveBindings();
