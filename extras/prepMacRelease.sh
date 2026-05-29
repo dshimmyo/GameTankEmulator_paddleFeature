827b0516766f589ba69a88dc4d9af9849d281b20
Daniel Shimmyo <dshimmyo@gmail.com>
2026-05-29 11:26:48 -0400

    update mac build tweak gitignore for windows build

---
+++ extras/prepMacRelease.sh
@@ -0,0 +1,3 @@
+cp /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib BrickGameMac/
+install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib @loader_path/libSDL2-2.0.0.dylib BrickGameMac/BrickGame
+otool -L BrickGameMac/BrickGame
