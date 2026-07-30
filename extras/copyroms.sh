945e48cf968144a99f0ac6b1d9bb6b5716589b8c
Daniel Shimmyo <dshimmyo@gmail.com>
2026-07-30 10:59:17 -0400

    copyroms shell command

---
+++ extras/copyroms.sh
@@ -0,0 +1,33 @@
+diff roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr 
+diff roms/brickgame.gtr BrickGameWin/gamedata.gtr 
+diff roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+diff roms/brickgame.gtr SteamStaging/content/windows_depot/gamedata.gtr 
+
+cp roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr 
+cp roms/brickgame.gtr BrickGameWin/gamedata.gtr 
+cp roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+cp roms/brickgame.gtr SteamStaging/content/windows_depot/gamedata.gtr 
+
+echo
+
+diff roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gamedata.gtr
+diff roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
+diff roms/BrickGameDemo.gtr SteamDemoStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+diff roms/BrickGameDemo.gtr SteamDemoStaging/content/windows_depot/gamedata.gtr
+
+cp roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gamedata.gtr
+cp roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
+cp roms/BrickGameDemo.gtr SteamDemoStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+cp roms/BrickGameDemo.gtr SteamDemoStaging/content/windows_depot/gamedata.gtr 
+
+echo
+
+diff roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr
+diff roms/brickgame.gtr BrickGameWin/gamedata.gtr 
+diff roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+diff roms/brickgame.gtr SteamStaging/content/windows_depot/gamedata.gtr 
+diff roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gamedata.gtr
+diff roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
+diff roms/BrickGameDemo.gtr SteamDemoStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
+diff roms/BrickGameDemo.gtr SteamDemoStaging/content/windows_depot/gamedata.gtr 
+
