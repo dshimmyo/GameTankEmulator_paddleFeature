e1f73f6d3208ae9effc308669b491d1a966e8730
Daniel Shimmyo <dshimmyo@gmail.com>
2026-09-01 12:14:37 -0400

    new batch script

---
+++ extras/copywindowsbuilds.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+cp .\build\GameTankEmulator.exe .\BrickGameWin\BrickGame.exe
+cp .\build\GameTankEmulator.exe .\BrickGameDemoWin\BrickGame.exe
+cp .\build\GameTankEmulator.exe .\SteamStaging\content\windows_depot\BrickGame.exe
+cp .\build\GameTankEmulator.exe .\SteamDemoStaging\content\windows_depot\BrickGame.exe
