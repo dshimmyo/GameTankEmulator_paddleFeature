c036a68019a3b08314312436576d15c0124331d8
Daniel Shimmyo <dshimmyo@gmail.com>
2026-08-31 21:08:25 -0400

    extras script tweak

--- extras/copyroms.sh
+++ extras/copyroms.sh
@@ -1,3 +1,5 @@
+echo "diffing roms/brickgame.gtr:"
+
 diff roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr 
 diff roms/brickgame.gtr BrickGameWin/gamedata.gtr 
 diff roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
@@ -8,7 +10,7 @@ cp roms/brickgame.gtr BrickGameWin/gamedata.gtr
 cp roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
 cp roms/brickgame.gtr SteamStaging/content/windows_depot/gamedata.gtr 
 
-echo
+echo "diffing roms/BrickGameDemo.gtr:"
 
 diff roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gamedata.gtr
 diff roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
@@ -20,7 +22,7 @@ cp roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr
 cp roms/BrickGameDemo.gtr SteamDemoStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
 cp roms/BrickGameDemo.gtr SteamDemoStaging/content/windows_depot/gamedata.gtr 
 
-echo
+echo "final diffs 8 files:"
 
 diff roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr
 diff roms/brickgame.gtr BrickGameWin/gamedata.gtr 
