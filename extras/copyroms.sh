013f5449d2827a7d25e914ba47810985995b6cf0
Daniel Shimmyo <dshimmyo@gmail.com>
2026-08-31 21:16:50 -0400

    script tweak

--- extras/copyroms.sh
+++ extras/copyroms.sh
@@ -5,6 +5,8 @@ diff roms/brickgame.gtr BrickGameWin/gamedata.gtr
 diff roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
 diff roms/brickgame.gtr SteamStaging/content/windows_depot/gamedata.gtr 
 
+echo "copying roms/brickgame.gtr:"
+
 cp roms/brickgame.gtr BrickGameMac/BrickGame.app/Contents/MacOS/gamedata.gtr 
 cp roms/brickgame.gtr BrickGameWin/gamedata.gtr 
 cp roms/brickgame.gtr SteamStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
@@ -16,6 +18,8 @@ diff roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gameda
 diff roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
 diff roms/BrickGameDemo.gtr SteamDemoStaging/content/mac_depot/BrickGame.app/Contents/MacOS/gamedata.gtr 
 diff roms/BrickGameDemo.gtr SteamDemoStaging/content/windows_depot/gamedata.gtr
+
+echo "copying roms/BrickGameDemo.gtr:"
 
 cp roms/BrickGameDemo.gtr BrickGameDemoMac/BrickGame.app/Contents/MacOS/gamedata.gtr
 cp roms/BrickGameDemo.gtr BrickGameDemoWin/gamedata.gtr 
